7f47150d4e7bbceab9b2fbe70fa17fd941f956f7
velmurugan-hue <velmurugan@silverbackev.com>
2026-09-18 09:19:31 +0530

    v0.004: align CAN1 state documentation with active detection

--- src/CAN/can1.h
+++ src/CAN/can1.h
@@ -75,8 +75,7 @@ extern "C" {
 
 typedef enum
 {
-    CAN1_STATE_DETECTING = 0,  /* LOM active, scanning for valid frames */
-    /* Internal loopback self-test */
+    CAN1_STATE_DETECTING = 0,  /* NORMAL/active, scanning for valid frames */
     CAN1_STATE_READY,          /* Candidate accepted, normal reception */
     CAN1_STATE_ERROR           /* Unrecoverable - re-detecting     */
 } Can1_State_t;
