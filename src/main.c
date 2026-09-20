e847f793bb8b4ff74d49e43298c1cffbfe658501
velmurugan-hue <velmurugan@silverbackev.com>
2026-09-20 20:43:48 +0530

    chore: update firmware banner to V0.0057

--- src/main.c
+++ src/main.c
@@ -420,8 +420,8 @@ int main(void)
     SEGGER_RTT_printf(0,
         "\r\n================================================\r\n"
         " Zitto MB V1 - VCU Firmware Boot\r\n"
-        " Firmware Revision : V0.0056\r\n"
-        " Change            : CAN1 V0.0056 CAN-service-isolated auto-baud analysis mode\r\n"
+        " Firmware Revision : V0.0057\r\n"
+        " Change            : CAN1 V0.0057 CAN-service-isolated auto-baud analysis mode\r\n"
         " MCU: S32K144  Clock: 80MHz SPLL  WDOG: OFF\r\n"
         " Modules: IMU=%d CSA=%d CAN1=%d CAN2=%d FLM=%d GPIO=%d\r\n"
         "================================================\r\n\r\n",
