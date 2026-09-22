3c58cf4473f86742e6df883dd01317df94c5c567
velmuruganv098 <58551408+velmuruganv098@users.noreply.github.com>
2026-09-22 17:45:09 +0530

    chore: remove .backups from v0.0064 CAN2 branch

--- .backups/2026-09-19_09-41-21_INITIAL/backup/flash.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Program Files\SEGGER\JLink\JLink.exe" -device S32K144 -if SWD -speed 1000 -CommanderScript "C:\Projects\Zitto_MB_V1\flash.jlink"
--- .backups/2026-09-19_09-42-50_INITIAL/flash.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Program Files\SEGGER\JLink\JLink.exe" -device S32K144 -if SWD -speed 1000 -CommanderScript "C:\Users\Velu\workspaceS32DS.3.4\Zitto_MB_V1\flash.jlink"
